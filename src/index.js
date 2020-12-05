9eb834a369376aecd38c7c63dd638d63ebc957ff
Mohammad H. Sattarian <mh_sattarian@outlook.com>
2020-12-05 15:51:08 +0330

    WIP: Fix issues on toolbars

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
+import MapboxDraw from '@mapbox/mapbox-gl-draw';
+
+import { SnapPolygonMode, SnapPointMode, SnapLineMode, SnapModeDrawStyles } from 'mapbox-gl-draw-snap-mode';
+import mapboxGlDrawPinningMode from 'mapbox-gl-draw-pinning-mode';
+import mapboxGlDrawPassingMode from 'mapbox-gl-draw-passing-mode';
+import { SRMode, SRCenter, SRStyle } from 'mapbox-gl-draw-scale-rotate-mode';
+import CutPolygonMode from 'mapbox-gl-draw-cut-polygon-mode';
+import SplitPolygonMode from 'mapbox-gl-draw-split-polygon-mode';
+import SplitLineMode from 'mapbox-gl-draw-split-line-mode';
 import additionalTools from 'mapbox-gl-draw-additional-tools';
+
 import './index.css';
 class SnapOptionsToolbar {
     constructor(opt) {
@@ -45,196 +55,213 @@ class SnapOptionsToolbar {
     }
 }
 
-class ExtendDrawBar {
-    constructor(opt) {
-        let ctrl = this;
-        ctrl.draw = opt.draw;
-        ctrl.buttons = opt.buttons || [];
-        ctrl.onAddOrig = opt.draw.onAdd;
-        ctrl.onRemoveOrig = opt.draw.onRemove;
-    }
-    onAdd(map) {
-        let ctrl = this;
-        ctrl.map = map;
-        ctrl.elContainer = ctrl.onAddOrig(map);
-        ctrl.buttons.forEach((b) => {
-            ctrl.addButton(b);
-        });
-        return ctrl.elContainer;
-    }
-    onRemove(map) {
-        ctrl.buttons.forEach((b) => {
-            ctrl.removeButton(b);
-        });
-        ctrl.onRemoveOrig(map);
-    }
-    addButton(opt) {
-        let ctrl = this;
-        var elButton = document.createElement('button');
-        elButton.className = 'mapbox-gl-draw_ctrl-draw-btn';
-        elButton.setAttribute('title', opt.title);
-        if (opt.classes instanceof Array) {
-            opt.classes.forEach((c) => {
-                elButton.classList.add(c);
+export default class BetterMapboxDraw extends MapboxDraw {
+    constructor(options) {
+        const { modes, otherOtions } = options;
+
+        const customModes = {
+            ...MapboxDraw.modes,
+            draw_point: SnapPointMode,
+            draw_polygon: SnapPolygonMode,
+            draw_line_string: SnapLineMode,
+            pinning_mode: mapboxGlDrawPinningMode,
+            passing_mode_point: mapboxGlDrawPassingMode(MapboxDraw.modes.draw_point),
+            passing_mode_line_string: mapboxGlDrawPassingMode(MapboxDraw.modes.draw_line_string),
+            passing_mode_polygon: mapboxGlDrawPassingMode(MapboxDraw.modes.draw_polygon),
+            scaleRotateMode: SRMode,
+            cutPolygonMode: CutPolygonMode,
+            splitPolygonMode: SplitPolygonMode,
+            splitLineMode: SplitLineMode,
+        };
+
+        const customOptions = {
+            bufferSize: 0.5,
+            bufferUnit: 'kilometers',
+            bufferSteps: 64,
+            snap: true,
+            // snapOptions: {
+            //   snapPx: 15,
+            //   snapToMidPoints: true,
+            // },
+            guides: false,
+            userProperties: true,
+        };
+
+        const _modes = { ...customModes, ...modes };
+        const _options = { modes: _modes, ...customOptions, ...otherOtions };
+
+        super(_options);
+
+        // this.draw = opt.draw;
+        this.buttons = [
+            {
+                on: 'click',
+                action: () => {
+                    try {
+                        draw.changeMode('splitLineMode', {
+                            spliter: prompt('Which Mode? (point, line_string, polygon)'),
+                        });
+                    } catch (err) {
+                        alert(err.message);
+                        console.error(err);
+                    }
+                },
+                classes: ['split-line'],
+            },
+            {
+                on: 'click',
+                action: () => {
+                    try {
+                        draw.changeMode('splitPolygonMode');
+                    } catch (err) {
+                        alert(err.message);
+                        console.error(err);
+                    }
+                },
+                classes: ['split-polygon'],
+            },
+            {
+                on: 'click',
+                action: () => {
+                    try {
+                        draw.changeMode('cutPolygonMode');
+                    } catch (err) {
+                        alert(err.message);
+                        console.error(err);
+                    }
+                },
+                classes: ['cut-polygon'],
+            },
+            {
+                on: 'click',
+                action: () => {
+                    try {
+                        draw.changeMode('scaleRotateMode', {
+                            // required
+                            canScale: true,
+                            canRotate: true, // only rotation enabled
+                            canTrash: false, // disable feature delete
+
+                            rotatePivot: SRCenter.Center, // rotate around center
+                            scaleCenter: SRCenter.Opposite, // scale around opposite vertex
+
+                            singleRotationPoint: true, // only one rotation point
+                            rotationPointRadius: 1.2, // offset rotation point
+
+                            canSelectFeatures: true,
+                        });
+                    } catch (err) {
+                        alert(err.message);
+                        console.error(err);
+                    }
+                },
+                classes: ['rotate-icon'],
+            },
+            {
+                on: 'click',
+                action: () => {
+                    draw.changeMode('pinning_mode');
+                },
+                classes: ['pinning_mode'],
+                title: 'Pinning Mode tool',
+            },
+            // {
+            //     on: 'click',
+            //     action: () => {
+            //         draw.changeMode('passing_mode_point');
+            //     },
+            //     classes: ['passing_mode', 'point'],
+            //     title: 'Passing-Point tool',
+            // },
+            // {
+            //     on: 'click',
+            //     action: () => {
+            //         draw.changeMode('passing_mode_line_string', (info) => {
+            //             console.log(info);
+            //         });
+            //     },
+            //     classes: ['passing_mode', 'line'],
+            //     title: 'Passing-LineString tool',
+            // },
+            // {
+            //     on: 'click',
+            //     action: () => {
+            //         draw.changeMode('passing_mode_polygon');
+            //     },
+            //     classes: ['passing_mode', 'polygon'],
+            //     title: 'Passing-Polygon tool',
+            // },
+        ];
+
+        this.onAddOrig = this.onAdd;
+        this.onRemoveOrig = this.onRemove;
+
+        const addOtherControls = async (map, draw, placement) => {
+            const snapOptionsBar = new SnapOptionsToolbar({
+                draw,
+                checkboxes: [
+                    {
+                        on: 'change',
+                        action: (e) => {
+                            draw.options.snap = e.target.checked;
+                        },
+                        classes: ['snap_mode', 'snap'],
+                        title: 'Snap when Draw',
+                        initialState: 'checked',
+                    },
+                    {
+                        on: 'change',
+                        action: (e) => {
+                            draw.options.guides = e.target.checked;
+                        },
+                        classes: ['snap_mode', 'grid'],
+                        title: 'Show Guides',
+                    },
+                ],
             });
-        }
-        elButton.addEventListener(opt.on, opt.action);
-        ctrl.elContainer.appendChild(elButton);
-        opt.elButton = elButton;
-    }
-    removeButton(opt) {
-        opt.elButton.removeEventListener(opt.on, opt.action);
-        opt.elButton.remove();
+
+            setTimeout(() => {
+                map.addControl(additionalTools(draw), placement);
+                map.addControl(snapOptionsBar, placement);
+            }, 400);
+        };
+
+        this.onAdd = (map, placement) => {
+            this.map = map;
+            this.elContainer = this.onAddOrig(map, placement);
+
+            this.buttons.forEach((b) => {
+                this.addButton(b);
+            });
+
+            addOtherControls(map, this, placement);
+            return this.elContainer;
+        };
+
+        this.onRemove = (map) => {
+            this.buttons.forEach((b) => {
+                this.removeButton(b);
+            });
+            this.onRemoveOrig(map);
+        };
+
+        this.addButton = (opt) => {
+            var elButton = document.createElement('button');
+            elButton.className = 'mapbox-gl-draw_ctrl-draw-btn';
+            elButton.setAttribute('title', opt.title);
+            if (opt.classes instanceof Array) {
+                opt.classes.forEach((c) => {
+                    elButton.classList.add(c);
+                });
+            }
+            elButton.addEventListener(opt.on, opt.action);
+            this.elContainer.appendChild(elButton);
+            opt.elButton = elButton;
+        };
+
+        this.removeButton = (opt) => {
+            opt.elButton.removeEventListener(opt.on, opt.action);
+            opt.elButton.remove();
+        };
     }
 }
-
-export default {
-    addControl(map, draw, placement) {
-        const snapOptionsBar = new SnapOptionsToolbar({
-            draw: draw,
-            checkboxes: [
-                {
-                    on: 'change',
-                    action: (e) => {
-                        draw.options.snap = e.target.checked;
-                    },
-                    classes: ['snap_mode', 'snap'],
-                    title: 'Snap when Draw',
-                    initialState: 'checked',
-                },
-                {
-                    on: 'change',
-                    action: (e) => {
-                        draw.options.guides = e.target.checked;
-                    },
-                    classes: ['snap_mode', 'grid'],
-                    title: 'Show Guides',
-                },
-            ],
-        });
-
-        const drawBar = new ExtendDrawBar({
-            draw: draw,
-            buttons: [
-                {
-                    on: 'click',
-                    action: () => {
-                        try {
-                            draw.changeMode('splitLineMode', {
-                                spliter: prompt('Which Mode? (point, line_string, polygon)'),
-                            });
-                        } catch (err) {
-                            alert(err.message);
-                            console.error(err);
-                        }
-                    },
-                    classes: ['split-line'],
-                },
-                {
-                    on: 'click',
-                    action: () => {
-                        try {
-                            draw.changeMode('splitPolygonMode');
-                        } catch (err) {
-                            alert(err.message);
-                            console.error(err);
-                        }
-                    },
-                    classes: ['split-polygon'],
-                },
-                {
-                    on: 'click',
-                    action: () => {
-                        try {
-                            draw.changeMode('cutPolygonMode');
-                        } catch (err) {
-                            alert(err.message);
-                            console.error(err);
-                        }
-                    },
-                    classes: ['cut-polygon'],
-                },
-                {
-                    on: 'click',
-                    action: () => {
-                        try {
-                            draw.changeMode('scaleRotateMode', {
-                                // required
-                                canScale: true,
-                                canRotate: true, // only rotation enabled
-                                canTrash: false, // disable feature delete
-
-                                rotatePivot: SRCenter.Center, // rotate around center
-                                scaleCenter: SRCenter.Opposite, // scale around opposite vertex
-
-                                singleRotationPoint: true, // only one rotation point
-                                rotationPointRadius: 1.2, // offset rotation point
-
-                                canSelectFeatures: true,
-                            });
-                        } catch (err) {
-                            alert(err.message);
-                            console.error(err);
-                        }
-                    },
-                    classes: ['rotate-icon'],
-                },
-                {
-                    on: 'click',
-                    action: () => {
-                        draw.changeMode('pinning_mode');
-                    },
-                    classes: ['pinning_mode'],
-                    title: 'Pinning Mode tool',
-                },
-                // {
-                //     on: 'click',
-                //     action: () => {
-                //         draw.changeMode('passing_mode_point');
-                //     },
-                //     classes: ['passing_mode', 'point'],
-                //     title: 'Passing-Point tool',
-                // },
-                // {
-                //     on: 'click',
-                //     action: () => {
-                //         draw.changeMode('passing_mode_line_string', (info) => {
-                //             console.log(info);
-                //         });
-                //     },
-                //     classes: ['passing_mode', 'line'],
-                //     title: 'Passing-LineString tool',
-                // },
-                // {
-                //     on: 'click',
-                //     action: () => {
-                //         draw.changeMode('passing_mode_polygon');
-                //     },
-                //     classes: ['passing_mode', 'polygon'],
-                //     title: 'Passing-Polygon tool',
-                // },
-            ],
-        });
-
-        map.addControl(drawBar, placement);
-        map.addControl(additionalTools(draw), placement);
-        map.addControl(snapOptionsBar, placement);
-    },
-
-    // patchModes(MapboxDrawModes) {
-    //     return {
-    //             ...MapboxDraw.modes,
-    //             draw_point: SnapPointMode,
-    //             draw_polygon: SnapPolygonMode,
-    //             draw_line_string: SnapLineMode,
-    //             pinning_mode: mapboxGlDrawPinningMode,
-    //             passing_mode_point: mapboxGlDrawPassingMode(MapboxDraw.modes.draw_point),
-    //             passing_mode_line_string: mapboxGlDrawPassingMode(MapboxDraw.modes.draw_line_string),
-    //             passing_mode_polygon: mapboxGlDrawPassingMode(MapboxDraw.modes.draw_polygon),
-    //             scaleRotateMode: SRMode,
-    //             cutPolygonMode: CutPolygonMode,
-    //             splitPolygonMode: SplitPolygonMode,
-    //             splitLineMode: SplitLineMode,
-    //     }
-    // }
-};
